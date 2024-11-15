57a8cf3e73a507e560c5b36d604f6f39305b7dae
César Centeno <CCentenor@gmail.com>
2024-11-14 22:10:39 -0500

    Operadores

---
+++ dia_01/01_variables.py
@@ -0,0 +1,24 @@
+numero1=20
+numero2=30
+#Aritmeticos
+suma= numero1 + numero2 #suma
+# Resta
+resta= numero1 - numero2
+# Multiplicacion
+multiplicacion=numero1*numero2
+# Division
+division=numero1/numero2
+# Division parte entera
+division_entera=numero1//numero2
+# Modulo
+modulo=numero1%numero2
+# Potencia
+potencia=numero1**numero2
+# Para poder realizar la raiz
+raiz_cuadrada=9 ** (1/2)
+
+print(division)
+print(division_entera)
+print(potencia)
+print(raiz_cuadrada)
+
