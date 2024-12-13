4ca2983bd5e8d954f8c9c910e6a12d81f19c5037
César Centeno <CCentenor@gmail.com>
2024-12-12 23:08:49 -0500

    Uso del login

--- controllers/serializers/usuario_serializer.py
+++ controllers/serializers/usuario_serializer.py
@@ -1,14 +1,21 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from models import Usuario, TipoUsuario
 from marshmallow_enum import EnumField
+from marshmallow import Schema, fields
 
 class RegistroSerializer(SQLAlchemyAutoSchema):
     # si quiero modificar alguna columna del modelo para cuestiones del serializador
     # ahora modificamos la columna password y le indicamos que tiene que ser requerida a la hora de serializar
-    password = auto_field(required=True)
+    # load_only > se usara para cargar de la bd
+    password = auto_field(required=True, load_only=True)
     # modificar el comportamiento de la columna que sea enum en la cual se le coloca que enum debe utilizar para hacer las validaciones correspondientes
 
     tipoUsuario = EnumField(TipoUsuario)
 
     class Meta:
         model = Usuario
+
+
+class LoginSerializer(Schema):
+    correo = fields.Email(required=True)
+    password = fields.String(required=True)
